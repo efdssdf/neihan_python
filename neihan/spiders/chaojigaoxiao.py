a6ff60a92a61b535650522139b9c4f5d979fc35d
efdssdf <efdssdf@gmail.com>
2018-09-07 19:03:10 +0800

    'fix'

--- neihan/spiders/chaojigaoxiao.py
+++ neihan/spiders/chaojigaoxiao.py
@@ -5,6 +5,7 @@ import datetime
 import time
 from neihan.items import NeihanItem
 import random
+n=0
 
 class QuotesSpider(scrapy.Spider):
     name = "chaojigaoxiao"
@@ -14,6 +15,8 @@ class QuotesSpider(scrapy.Spider):
         yield scrapy.Request(url=url,callback=self.parse,dont_filter=True)
 
     def parse(self, response):
+        global n
+        n+=1
         d = eval(response.body.decode('utf-8'))
         for i in d['data']:
             mktime = time.mktime(datetime.datetime.now().timetuple())
@@ -25,7 +28,7 @@ class QuotesSpider(scrapy.Spider):
             item['count'] = random.randint(200,1000)
             yield item
         else:
-            if len(d['data'])>0:
+            if len(d['data'])>0 and n<100:
                 url = "https://video.beanmedia.cn/api/gifs?format=json"
                 yield scrapy.Request(url=url,callback=self.parse,dont_filter=True)
             else:
